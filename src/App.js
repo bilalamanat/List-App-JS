9de06ed112ce9c5ea05c4ca069775f1d1c7bf43a
Bilal Amanat Muhammadi <136390430+bilalamanat@users.noreply.github.com>
2023-08-13 21:57:54 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><em>TO DO LIST APP</em></h1>
+      <input input type='text' placeholder='ADD TASK'
+      ></input>
+      <button><em>ADD TASK</em></button>
     </div>
   );
 }
