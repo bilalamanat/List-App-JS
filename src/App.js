5e87773b07ac5ff40fc1cba91ea1fba8201befda
Bilal Amanat Muhammadi <136390430+bilalamanat@users.noreply.github.com>
2023-08-17 21:44:05 +0500

    hi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [newtask,setNewtask]= useState("")
+  const handleaddtask = () =>{
+setNewtask() 
+  }
   return (
     <div className="App">
       <h1><em>TO DO LIST APP</em></h1>
       <input input type='text' placeholder='ADD TASK'
-      ></input>
+     onChange={handleaddtask} ></input>
       <button><em>ADD TASK</em></button>
     </div>
   );
